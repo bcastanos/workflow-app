ab836a846e7a430cbdd8db69165552edd118e90c
Bryan Castanos <bcastanos@gmail.com>
2016-03-04 12:06:06 -0800

    added root route

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ var express = require('express')
 var mongoose  = require('mongoose')
 var app = express()
 
+app.get('/', function(req,res){
+  res.send("Welcome home.")
+})
+
 app.listen(3000, function(){
   console.log('listening on port 3000')
 })
