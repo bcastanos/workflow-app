5a4be78edec1fbcb0cb2d7c71afe3aa2ba5d8555
Laura Leonard <Laura.Leonard818@gmail.com>
2016-03-04 12:08:09 -0800

    added body-parser and about route

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 var express = require('express')
 var mongoose  = require('mongoose')
 var app = express()
+var bodyParser = require('body-parser')
 
+app.use(bodyParser.json())
+
+app.get('/about', function(req,res){
+  res.send('This is the about page')
+})
 app.listen(3000, function(){
   console.log('listening on port 3000')
 })
